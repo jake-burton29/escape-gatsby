289cb5b61eba2709c1792e95b68cc1ca0ebad2b6
jake-burton29 <jacob.burton29@gmail.com>
2023-03-23 20:48:50 -0600

    get out of here Formik, update your deepMerge

--- src/pages/reviews.js
+++ src/pages/reviews.js
@@ -1,5 +1,4 @@
 import { graphql, useStaticQuery } from "gatsby";
-import { getImage } from "gatsby-plugin-image";
 import React from "react";
 import Layout from "../components/Layout";
 import ReviewCard from "../components/ReviewCard";
@@ -33,10 +32,6 @@ const Reviews = () => {
   console.log(data.allDataJson.edges[0].node.reviews);
   return (
     <Layout>
-      {data.allDataJson.edges.forEach((review) => {
-        const image = getImage(review.image);
-        console.log(image);
-      })}
       {data.allDataJson.edges[0].node.reviews.map((review) => (
         <ReviewCard key={review.id} review={review} image={review.image} />
       ))}
