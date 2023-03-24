ff4a9ded77f09e4d5bf8e7e7b21e4f7bd9ffa4b3
jake-burton29 <jacob.burton29@gmail.com>
2023-03-24 13:04:52 -0600

    some reviewCard styling

--- src/components/ReviewCard.jsx
+++ src/components/ReviewCard.jsx
@@ -2,24 +2,64 @@ import React from "react";
 import { getImage, GatsbyImage } from "gatsby-plugin-image";
 
 const ReviewCard = ({ review, image }) => {
-  const { title, rating, author, content, platform } = review;
-  const gatsbyImage = getImage(image);
-  console.log(image);
+  const { title, rating, author, content, platform, link, date } = review;
+  console.log(title);
   return (
-    <>
-      <h1>{title}</h1>
-      <p>Rating: {rating}</p>
-      <p>Author: {author}</p>
-      <p> {content}</p>
-      <p>From: {platform}</p>
+    <div className="card lg:card-side bg-slate-100 shadow-xl text-black my-6 max-w-full md:mx-24 pb-0 ">
+      <div className="card-body">
+        <a
+          href={link}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="card-title underline"
+        >
+          {title}
+        </a>
+        <p>{author}</p>
+        {rating === 5 && (
+          <div className="rating">
+            <input
+              disabled
+              name="rating2"
+              className="mask mask-star-2 bg-green-500"
+            ></input>
+            <input
+              disabled
+              name="rating2"
+              className="mask mask-star-2 bg-green-500"
+            ></input>
+            <input
+              disabled
+              name="rating2"
+              className="mask mask-star-2 bg-green-500"
+            ></input>
+            <input
+              disabled
+              name="rating2"
+              className="mask mask-star-2 bg-green-500"
+            ></input>
+            <input
+              disabled
+              name="rating2"
+              className="mask mask-star-2 bg-green-500"
+            ></input>
+          </div>
+        )}
+        <p> {content}</p>
+        <p>Date: {date}</p>
+        <p>From: {platform}</p>
+      </div>
       {image && (
-        <GatsbyImage
-          loading="lazy"
-          image={getImage(image.src.childImageSharp.gatsbyImageData)}
-          alt={image.alt}
-        />
+        <figure className="w-2/3 mx-auto rounded-t-xl">
+          <GatsbyImage
+            loading="lazy"
+            className="min-w-full min-h-fit aspect-square"
+            image={getImage(image.src.childImageSharp.gatsbyImageData)}
+            alt={image.alt}
+          />
+        </figure>
       )}
-    </>
+    </div>
   );
 };
 
--- src/pages/reviews.js
+++ src/pages/reviews.js
@@ -9,17 +9,19 @@ const Reviews = () => {
         edges {
           node {
             reviews {
+              title
               author
               content
               date
               id
               platform
               rating
+              link
               image {
                 alt
                 src {
                   childImageSharp {
-                    gatsbyImageData
+                    gatsbyImageData(width: 300, height: 300)
                   }
                 }
               }
@@ -32,6 +34,16 @@ const Reviews = () => {
   console.log(data.allDataJson.edges[0].node.reviews);
   return (
     <Layout>
+      <div className="hero min-w-screen bg-base-200 bg-inherit py-5">
+        <div className="hero-content text-center">
+          <div className="max-w-md">
+            <h1 className="text-4xl font-bold bg-clip-text bg-gradient-to-br from-yellow-300 via-orange-400 to-red-500 text-transparent">
+              Reviews
+            </h1>
+            <p className="py-1">Some reviews from our incredible customers!</p>
+          </div>
+        </div>
+      </div>
       {data.allDataJson.edges[0].node.reviews.map((review) => (
         <ReviewCard key={review.id} review={review} image={review.image} />
       ))}
